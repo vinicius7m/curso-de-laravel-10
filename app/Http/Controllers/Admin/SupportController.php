8e99493f162b467faf56eaf70589201b497c3924
Vinicius Moreira <vinicius.vsm777@gmail.com>
2023-08-14 17:51:47 -0300

    learning about models

--- app/Http/Controllers/Admin/SupportController.php
+++ app/Http/Controllers/Admin/SupportController.php
@@ -3,12 +3,17 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Support;
 use Illuminate\Http\Request;
 
 class SupportController extends Controller
 {
-    public function index()
+    public function index(Support $support /* Injeção de dependência do Laravel */ )
     {
-        return view('admin/supports/index');
+        // $support = new Support();
+        $supports = $support->all(); // Gera uma collection (Array)
+        // dd($supports);
+
+        return view('admin/supports/index', compact('supports'));
     }
 }
