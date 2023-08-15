88069a40a2ca3a4def1e66e906b8ae5b85d50be5
Vinicius Moreira <vinicius.vsm777@gmail.com>
2023-08-15 13:35:54 -0300

    feature: delete support

--- app/Http/Controllers/Admin/SupportController.php
+++ app/Http/Controllers/Admin/SupportController.php
@@ -71,4 +71,15 @@ class SupportController extends Controller
 
         return redirect()->route('supports.index');
     }
+
+    public function destroy(string|int $id)
+    {
+        if(!$support = Support::find($id)) {
+            return back();
+        }
+
+        $support->delete();
+
+        return redirect()->route('supports.index');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -4,16 +4,8 @@ use App\Http\Controllers\Site\SiteController;
 use App\Http\Controllers\Admin\{SupportController};
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+
+Route::delete('/supports/{id}', [SupportController::class, 'destroy'])->name('supports.destroy');
 Route::put('/supports/{id}', [SupportController::class, 'update'])->name('supports.update');
 Route::get('/supports/{id}/edit', [SupportController::class, 'edit'])->name('supports.edit');
 Route::get('/supports', [SupportController::class, 'index'])->name('supports.index');
