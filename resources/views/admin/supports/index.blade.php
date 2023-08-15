e6e7b394f153306d1c594b70faf3b94219f5699c
Vinicius Moreira <vinicius.vsm777@gmail.com>
2023-08-15 10:40:27 -0300

    feature: sign, read page support

--- resources/views/admin/supports/index.blade.php
+++ resources/views/admin/supports/index.blade.php
@@ -1,1 +1,21 @@
 <h1>Listagem dos suportes</h1>
+
+<a href="{{ route('supports.create') }}">Criar dúvida</a>
+
+<table>
+    <thead>
+        <th>assunto</th>
+        <th>status</th>
+        <th>descrição</th>
+        <th></th>
+    </thead>
+    <tbody>
+        @foreach($supports as $support)
+            <tr>
+                <td>{{ $support->subject }}</td>
+                <td>{{ $support->status }}</td>
+                <td>{{ $support->body }}</td>
+            </tr>
+        @endforeach
+    </tbody>
+</table>
